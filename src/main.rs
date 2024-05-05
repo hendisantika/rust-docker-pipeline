182347a71fd15a36884eee0db9b81aed0ce88a0f
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 11:15:40 +0700

    Modify main.rs 🫸🌀✏️📗🐧🐳

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("Hello, world!");
+    println!("Change this string to trigger recompilation instead of using Docker cache layers");
+    println!("{:}", chrono::Utc::now())
 }
